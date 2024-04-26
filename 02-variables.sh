44bc22886acd422319dc690ccd689fd1e913698f
vani135 <vanipipallapali@gmail.com>
2024-04-27 01:11:13 +0530

    hello-script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-thsi is again sample script
+#! /bib/bash
+
+echo "Rams:: Hello Sams, how is going on?"
+echo "Sams::Hi Rams, fine, how about you?"
+echo "Rams: I am good. bit busy in work"
+echo "Sams:: same here"
