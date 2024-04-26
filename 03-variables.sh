a397960b60b3a14e083df1db070d2859c4decf2a
vani135 <vanipipallapali@gmail.com>
2024-04-27 01:25:24 +0530

    hello-script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #! /bib/bash
 
-PERSON1=Rams #no space between = and value
-PERSON2=Sams
+PERSON1=Mimi #no space between = and value
+PERSON2=Simi
 
 echo "$PERSON1:: Hello $PERSON2, how is going on?"
 echo "$PERSON2::Hi $PERSON1, fine, how about you?"
